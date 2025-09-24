fd1e9257e2a5bb89c4dfbdbc86d6ee588a9bfda7
thnhu <hnhu3050@gmail.com>
2025-09-24 23:56:13 +0700

    model product

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,10 +1,35 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
 
-# Abstract base class for common fields and behavior
-class BaseModel(models.Model):
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Collection(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class ProductSize(models.Model):
+    EXTRA_SMALL = 1
+    SMALL = 2
+    MEDIUM = 3
+    LARGE = 4
+    EXTRA_LARGE = 5
+    SIZE_CHOICES = (
+        (EXTRA_SMALL, 'XS'),
+        (SMALL, 'S'),
+        (MEDIUM, 'M'),
+        (LARGE, 'L'),
+        (EXTRA_LARGE, 'XL'),
+    )
+    size = models.IntegerField(choices=SIZE_CHOICES, default=SMALL)
+
+class ProductDetail(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
