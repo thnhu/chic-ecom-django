57ac8ab1192d5dd6d61de25f5ab0c2f3c7c45e7b
thnhu <hnhu3050@gmail.com>
2025-09-25 14:34:03 +0700

    Rename app to products

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+    'products',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
--- products/models.py
+++ products/models.py
@@ -1,9 +1,4 @@
 from django.db import models
-from django.contrib.auth.hashers import make_password
-
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -13,7 +8,15 @@ class Collection(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
-class ProductSize(models.Model):
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, null=True)
+
+class ProductVariant(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     EXTRA_SMALL = 1
     SMALL = 2
     MEDIUM = 3
@@ -27,9 +30,5 @@ class ProductSize(models.Model):
         (EXTRA_LARGE, 'XL'),
     )
     size = models.IntegerField(choices=SIZE_CHOICES, default=SMALL)
-
-class ProductDetail(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
