6c096f991fe6caf33e5bf77a4e272b6215e0b3a6
thnhu <hnhu3050@gmail.com>
2025-09-26 15:14:48 +0700

    btn element

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,9 +28,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products',
+    'sass_processor',
     'rest_framework',
     'rest_framework_simplejwt',
+    'products',
 ]
 
 MIDDLEWARE = [
