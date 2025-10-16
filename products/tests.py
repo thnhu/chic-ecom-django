e99c6bae154ff6fd6dcdb8cd11835f658daf4ac1
thnhu <hnhu3050@gmail.com>
2025-10-16 19:39:55 +0700

    feat: breadcum feature

--- products/tests.py
+++
@@ -1,7 +0,0 @@
-# from django.test import TestCase
-# from .models import Product
-
-# class ProductModelTest(TestCase):
-#     def test_price_validation(self):
-#         product = Product(name='Test', base_price=100.00)
-#         self.assertTrue(product.base_price > 0)  # Validation logic đơn giản
